3f1d4694fcfaacf5de2a1f05b4074edefc7acd1c
Alex <akomanov@seatgeek.com>
2022-05-29 13:30:31 +0300

    Changing declaration of var to let in the writer.js

--- writer.js
+++ writer.js
@@ -1,5 +1,5 @@
 'use strict';
-var fs = require('fs-extra'),
+let fs = require('fs-extra'),
     path = require('path'),
     uuid = require('uuid'),
     xml = require('js2xmlparser');
@@ -9,7 +9,7 @@ module.exports = {
         fs.outputFileSync(path.join(targetDir, uuid.v4() + '-testsuite.xml'), xml.parse('ns2:test-suite', suites.toXML()));
     },
     writeBuffer: function(targetDir, buffer, ext) {
-        var fileName = uuid.v4() + '-attachment.' + ext;
+        let fileName = uuid.v4() + '-attachment.' + ext;
         fs.outputFileSync(path.join(targetDir, fileName), buffer);
         return fileName;
     }
